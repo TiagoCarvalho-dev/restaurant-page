b8f5a193453bb6589202cc83f7803e3b4c2e9445
Tiago de Siqueira Carvalho <tiagosiqueira.dev@gmail.com>
2023-09-28 20:22:16 -0300

    Added function to home.js
    The function creates the nav bar elements.

--- src/home.js
+++ src/home.js
@@ -7,9 +7,32 @@ function buildHeader() {
   return header;
 }
 
+function buildNav() {
+  const nav = document.createElement('nav');
+
+  const homeButton = document.createElement('button');
+  homeButton.classList.add('home-button');
+  homeButton.textContent = 'HOME';
+
+  const menuButton = document.createElement('button');
+  menuButton.classList.add('menu-button');
+  menuButton.textContent = 'MENU';
+
+  const contactButton = document.createElement('button');
+  contactButton.classList.add('contact-button');
+  contactButton.textContent = 'CONTACT';
+
+  nav.appendChild(homeButton);
+  nav.appendChild(menuButton);
+  nav.appendChild(contactButton);
+
+  return nav;
+}
+
 function buildHomePage() {
   const content = document.querySelector('#content');
   content.appendChild(buildHeader());
+  content.appendChild(buildNav());
 }
 
 export default buildHomePage;
