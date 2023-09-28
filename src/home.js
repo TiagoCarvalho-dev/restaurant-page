5d68ad7d630cbb41a99ffce949a53dc6d8b67ea7
Tiago de Siqueira Carvalho <tiagosiqueira.dev@gmail.com>
2023-09-28 20:57:25 -0300

    New functions to the home.js
    Added the functions to create the DOM elements for the main portion of the page and the footes.

--- src/home.js
+++ src/home.js
@@ -29,10 +29,89 @@ function buildNav() {
   return nav;
 }
 
+function buildMain() {
+  const mainDiv = document.createElement('div');
+  mainDiv.classList.add('main');
+
+  const h2 = document.createElement('h2');
+  h2.textContent = 'A lot of stuff will go here';
+
+  mainDiv.appendChild(h2);
+
+  const scheduleDiv = document.createElement('div');
+  scheduleDiv.classList.add('schedule-container');
+
+  const ul = document.createElement('ul');
+
+  const hoursLi = document.createElement('li');
+  hoursLi.textContent = 'Hours';
+  const mondayLi = document.createElement('li');
+  mondayLi.textContent = 'Monday';
+  const tuesdayLi = document.createElement('li');
+  tuesdayLi.textContent = 'Tuesday';
+  const wednesdayLi = document.createElement('li');
+  wednesdayLi.textContent = 'Wednesday';
+  const thursdayLi = document.createElement('li');
+  thursdayLi.textContent = 'Thursday';
+  const fridayLi = document.createElement('li');
+  fridayLi.textContent = 'Friday';
+  const saturdayLi = document.createElement('li');
+  saturdayLi.textContent = 'Saturday';
+  const sundayLi = document.createElement('li');
+  sundayLi.textContent = 'Sunday';
+
+  ul.appendChild(hoursLi);
+  ul.appendChild(mondayLi);
+  ul.appendChild(tuesdayLi);
+  ul.appendChild(wednesdayLi);
+  ul.appendChild(thursdayLi);
+  ul.appendChild(fridayLi);
+  ul.appendChild(saturdayLi);
+  ul.appendChild(sundayLi);
+
+  scheduleDiv.appendChild(ul);
+  mainDiv.appendChild(scheduleDiv);
+
+  return mainDiv;
+}
+
+function buildFooter() {
+  const footer = document.createElement('div');
+  footer.classList.add('footer');
+
+  const copyrightP = document.createElement('p');
+  copyrightP.textContent = 'Copyright ©';
+
+  const yearP = document.createElement('p');
+  yearP.textContent = new Date().getFullYear();
+
+  const gitHubPageP = document.createElement('p');
+  gitHubPageP.textContent = 'TiagoCarvalho-dev';
+
+  const a = document.createElement('a');
+  a.setAttribute('href', 'https://github.com/TiagoCarvalho-dev');
+
+  const img = document.createElement('img');
+  img.setAttribute('src', '../src/images/github.svg');
+  img.setAttribute('alt', 'Github Logo');
+  img.setAttribute('width', '30px');
+  img.setAttribute('height', 'auto');
+
+  a.appendChild(img);
+  footer.appendChild(copyrightP);
+  footer.appendChild(yearP);
+  footer.appendChild(gitHubPageP);
+  footer.appendChild(a);
+
+  return footer;
+}
+
 function buildHomePage() {
   const content = document.querySelector('#content');
   content.appendChild(buildHeader());
   content.appendChild(buildNav());
+  content.appendChild(buildMain());
+  content.appendChild(buildFooter());
 }
 
 export default buildHomePage;
